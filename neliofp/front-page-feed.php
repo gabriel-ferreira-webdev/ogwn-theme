b46e1db6b795e5ae235dd91241b827b01e45d777
Gabriel <contact@gabrielgf.com>
2020-10-11 17:06:23 -0300

    added multiple categories and implemented category color plugin

--- neliofp/front-page-feed.php
+++ neliofp/front-page-feed.php
@@ -12,13 +12,19 @@ global $post;
   <ul>
       <li class="feed-post featured-post">
         <!-- post categories -->
+        <div class="feed-categories">
+        <?php
+        $categories = get_the_category();
+        for ($i=0; $i < count($categories); $i++) {
+        $cat = $categories[$i];
+        $rl_category_color = rl_color($cat->term_id);
+        ?>
         <div class="feed-category">
-          <?php
-          $categories = get_the_category();
-          $cat = $categories[0];
-          ?>
-          <a href="<?php echo get_category_link($cat->term_id);?>"><?php echo $cat->name;?></a>
+
+          <a  title="<?php echo $cat->name;?>" href="<?php echo get_category_link($cat->term_id);?>" style="color:<?php echo $rl_category_color?>;"><?php echo $cat->name;?></a>
         </div>
+        <?php  } ?>
+              </div>
         <!-- post thumbnail -->
         <div class="feed-post-thumb featured-post-thumb">
           <a href="<?php the_permalink() ?>" rel="bookmark" class="feed-post-thumb-a" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></a>
@@ -27,11 +33,11 @@ global $post;
         <!-- post info -->
         <div class="feed-post-info-flex featured-post-info-flex">
 
-          <a class="feed-post-avatar featured-post-avatar" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( $author_id, '60' );?></a>
+          <a class="feed-post-avatar featured-post-avatar" title="<?php echo get_author_name($author_id);?>" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( $author_id, '60' );?></a>
 
           <div class="feed-post-info featured-post-info">
             <a href="<?php the_permalink(); ?>" rel="bookmark" class="feed-post-title-link"><span class="feed-post-title" title="<?php echo get_the_title(); ?>"><?php echo get_the_title();?></span></a>
-            <a class="feed-post-author-name" href="<?php echo get_author_posts_url($author_id); ?>"><?php echo get_author_name($author_id);?></a>
+            <a class="feed-post-author-name" title="<?php echo get_author_name($author_id);?>" href="<?php echo get_author_posts_url($author_id); ?>"><?php echo get_author_name($author_id);?></a>
           </div>
 
         </div>
--- page-all-content.php
+++ page-all-content.php
@@ -18,13 +18,19 @@
 
     <li class="feed-post">
       <!-- post categories -->
+      <div class="feed-categories">
+      <?php
+      $categories = get_the_category();
+      for ($i=0; $i < count($categories); $i++) {
+      $cat = $categories[$i];
+      $rl_category_color = rl_color($cat->term_id);
+      ?>
       <div class="feed-category">
-        <?php
-        $categories = get_the_category();
-        $cat = $categories[0];
-        ?>
-        <a href="<?php echo get_category_link($cat->term_id);?>"><?php echo $cat->name;?></a>
+
+        <a  title="<?php echo $cat->name;?>" href="<?php echo get_category_link($cat->term_id);?>" style="color:<?php echo $rl_category_color?>;"><?php echo $cat->name;?></a>
       </div>
+      <?php  } ?>
+            </div>
       <!-- post thumbnail -->
       <div class="feed-post-thumb ">
         <a href="<?php the_permalink() ?>" rel="bookmark" class="feed-post-thumb-a" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></a>
@@ -33,11 +39,11 @@
       <!-- post info -->
       <div class="feed-post-info-flex">
 
-        <a class="feed-post-avatar" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( get_the_author_email(), '60' );?></a>
+        <a class="feed-post-avatar" title="<?php the_author();?>" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( get_the_author_email(), '60' );?></a>
 
         <div class="feed-post-info">
           <a href="<?php the_permalink(); ?>" rel="bookmark" class="feed-post-title-link"><span class="feed-post-title" title="<?php echo get_the_title(); ?>"><?php echo get_the_title();?></span></a>
-          <a class="feed-post-author-name" href="<?php echo get_author_posts_url($author_id); ?>"><?php the_author();?></a>
+          <a class="feed-post-author-name" title="<?php the_author();?>"href="<?php echo get_author_posts_url($author_id); ?>"><?php the_author();?></a>
         </div>
 
       </div>
