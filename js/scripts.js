f08e7f1f1dd5091ed5864ca9bbd7ff2c136300f4
Kris Nelson <kris.belanger-nelson@telus.com>
2020-10-17 13:01:24 -0400

    search fixes
    mobile
    - text input larger, button larger
    - text input border
    Changed camelCase class/id names

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,3 @@
-// $(document).ready(function(){
-//   featuredCb.checked = false;
-// })
-// var featuredMore = document.querySelector(".more-featured");
-// var featuredPosts = document.querySelector(".featured-posts");
-// var featuredCb = document.querySelector("#featured-checkbox");
-// console.log(featuredMore);
-// console.log(featuredPosts);
-// console.log(featuredCb);
-//
-// featuredMore.addEventListener("click", function(){
-// if (featuredCb.checked) {
-//   featuredCb.checked = false;
-//     featuredPosts.style.paddingTop = "69%";
-// featuredMore.innerHTML = "See more..."
-// }else{
-//   featuredCb.checked = true;
-//     featuredPosts.style.paddingTop = "413%";
-//     featuredMore.innerHTML = "See less..."
-// }
-// })
-
-
-
-// DOTDOTDOT
-// var videoTitles = document.querySelectorAll( ".featured-post-title");
-// console.log(document.querySelector( ".featured-post-title"));
-// for (var i = 0; i < videoTitles.length; i++) {
-// console.log(videoTitles[i]);
-//   document.addEventListener( "DOMContentLoaded", () => {
-//      let wrapper = videoTitles[i];
-//      let options = {
-//        truncate: "letter",
-//         watch: true,
-//      };
-//      new Dotdotdot( wrapper, options );
-//   });
-// }
-
 /*
 // MOBILE MENU
 var menu = document.querySelector( ".menu-top-menu-container");
@@ -120,6 +81,6 @@ function initAuthor() {
 
 // Mobile menu
 function noScrollOnMenuOpen(checkbox) {
-  if (checkbox.checked) document.body.classList.add('noScrollOnMenuOpen' + checkbox.id);
-  else document.body.classList.remove('noScrollOnMenuOpen' + checkbox.id);
+  if (checkbox.checked) document.body.classList.add('no-scroll-menu-open' + '-' +  checkbox.id);
+  else document.body.classList.remove('no-scroll-menu-open' + '-' + checkbox.id);
 }
