6bc6d375d2174321c7e92400b49036e724832319
Kris Nelson <kris.belanger-nelson@telus.com>
2020-10-17 13:38:49 -0400

    fixed search value to show in input field

--- header.php
+++ header.php
@@ -18,40 +18,33 @@
         <a href="<?php echo get_home_url(); ?>" class="title-img"></a>
         <?php echo get_theme_mod( 'my_site_logo_id' );?>
 
+        <form action="/" method="get" id="searchform" class="header-searchform">
+          <input type="checkbox" name="search-cb" id="search-cb" onclick="noScrollOnMenuOpen(this)" />
+          <label id="search-cb-label" for="search-cb">
+            <svg class="search-icon search-open" viewBox="0 0 24 24">
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+              <path d="M0 0h24v24H0z" fill="none"/>
+            </svg>
+          </label>
 
-          <form action="/" method="get" id="searchform" class="header-searchform">
-            <input type="checkbox" name="search-cb" id="search-cb" onclick="noScrollOnMenuOpen(this)">
-            <label id="search-cb-label" for="search-cb">
+          <div class="search-bg-mb" id="search-bg-mb">
+            <input type="text" name="s" id="search" placeholder="Search" size="17" value="<?php echo get_search_query(); ?>" />
 
-              <svg class="search-icon search-open" viewBox="0 0 24 24">
+            <button type="submit" id="search-button">
+              <svg id="search-icon" class="search-icon" viewBox="0 0 24 24">
                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
                 <path d="M0 0h24v24H0z" fill="none"/>
               </svg>
+            </button>
+          </div>
+        </form>
 
-            </label>
-            <div class="search-bg-mb" id="search-bg-mb">
-              <input type="text" name="s" id="search" placeholder="Search" size="17"/>
-
-              <button type="submit" id="search-button">
-                <svg id="search-icon" class="search-icon" viewBox="0 0 24 24">
-                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                  <path d="M0 0h24v24H0z" fill="none"/>
-                </svg>
-               </button>
-
-
-            </div>
-          </input>
-
-
-             </form>
         <label for="search-cb" class="fade" id="fadesearch"></label>
 
         <nav id="hamnav">
           <!-- [THE HAMBURGER] -->
-          <input type="checkbox" id="hammy" name="hammy" onclick="noScrollOnMenuOpen(this)">
-            <label for="hammy">&#9776;</label>
-          </input>
+          <input type="checkbox" id="hammy" name="hammy" onclick="noScrollOnMenuOpen(this)" />
+          <label for="hammy">&#9776;</label>
 
           <!-- [MENU ITEMS] -->
           <div id="hamitems">
