93ed28eb523dcaa7ed94f8967d5c180dc12a2915
Kris Nelson <kris.belanger-nelson@telus.com>
2020-10-17 14:32:46 -0400

    DRY feed-post in content, search, author

--- author.php
+++ author.php
@@ -84,27 +84,7 @@ $author_name = get_author_name();
           <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
           <?php $author_id = get_the_author_meta('ID'); ?>
 
-          <!-- Post -->
-          <li class="feed-post">
-
-            <!-- post thumbnail -->
-            <div class="feed-post-thumb ">
-              <a href="<?php the_permalink() ?>" rel="bookmark" class="feed-post-thumb-a" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></a>
-            </div>
-
-            <!-- post info -->
-            <!-- <div class="feed-post-info-flex"> -->
-
-
-                      <div class="feed-post-info ">
-                        <a href="<?php the_permalink(); ?>" rel="bookmark" class="feed-post-title-link"><span class="feed-post-title" title="<?php echo get_the_title(); ?>"><?php echo get_the_title();?></span></a>
-                                    <a class="feed-post-avatar " title="<?php echo get_author_name($author_id);?>" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( $author_id, '60' );?></a>
-                        <a class="feed-post-author-name" title="<?php echo get_author_name($author_id);?>" href="<?php echo get_author_posts_url($author_id); ?>"><?php echo get_author_name($author_id);?></a>
-                      </div>
-
-            <!-- </div> -->
-
-          </li>
+          <?php include("feed-post.php"); ?>
           <?php endwhile; wp_reset_postdata();?>
         </ul>
       </div> <!-- Posts feed -->
--- page-all-content.php
+++ page-all-content.php
@@ -14,23 +14,7 @@
           <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
           <?php $author_id = get_the_author_meta('ID'); ?>
 
-          <li class="feed-post">
-            <!-- post thumbnail -->
-            <div class="feed-post-thumb ">
-              <a href="<?php the_permalink() ?>" rel="bookmark" class="feed-post-thumb-a" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></a>
-            </div>
-
-            <!-- post info -->
-            <div class="feed-post-info-flex">
-              <a class="feed-post-avatar" title="<?php the_author();?>" href="<?php echo get_author_posts_url($author_id); ?>"> <?php echo get_avatar( get_the_author_email(), '60' );?></a>
-
-              <div class="feed-post-info">
-                <a href="<?php the_permalink(); ?>" rel="bookmark" class="feed-post-title-link"><span class="feed-post-title" title="<?php echo get_the_title(); ?>"><?php echo get_the_title();?></span></a>
-                <a class="feed-post-author-name" title="<?php the_author();?>"href="<?php echo get_author_posts_url($author_id); ?>"><?php the_author();?></a>
-              </div>
-
-            </div>
-          </li>
+          <?php include("feed-post.php"); ?>
         <?php endwhile;wp_reset_postdata();?>
         </ul>
       </div>    <!-- Posts feed -->
