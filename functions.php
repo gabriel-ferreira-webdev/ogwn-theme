f44305b4f8f7547da5a445a421291239795a2439
Kris Nelson <kris.belanger-nelson@telus.com>
2020-10-17 14:43:00 -0400

    comment out include jquery (not used)

--- functions.php
+++ functions.php
@@ -7,13 +7,14 @@ function load_stylesheets()
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
+/* JQuery not being used, should avoid using it in future :P
 function include_jquery(){
   wp_deregister_script('jquery');
   wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.5.1.min.js', '', 1, true);
   add_action('wp_enqueue_scripts', 'jquery');
 }
 add_action('wp_enqueue_scripts', 'include_jquery');
-
+*/
 
 function loadjs(){
   wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
